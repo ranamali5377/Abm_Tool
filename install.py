8d4ff5ddd202f97200995fd62d61ad1bea12d881
Abdul Basit Minhas <52023076+Tech-abm@users.noreply.github.com>
2021-10-10 23:03:05 -0700

    Update install.py

--- install.py
+++ install.py
@@ -4,36 +4,22 @@ import re
 import time
 import platform
 
-logo = """
-     ___    ____  __  ___   __________  ____  __ 
-    /   |  / __ )/  |/  /  /_  __/ __ \/ __ \/ / 
-   / /| | / __  / /|_/ /    / / / / / / / / / /  
-  / ___ |/ /_/ / /  / /    / / / /_/ / /_/ / /___
- /_/  |_/_____/_/  /_/    /_/  \____/\____/_____/
---------------------------------------------------
-(~) Author  : Tech Abm
-(~) Github  : https://github.com/Tech-abm
-(~) Fb Page : https://facebook.com/Techabm
---------------------------------------------------                                             
-"""
+try:
+     import bs4
+except IOError:
+     os.system("pip2 install requests bs4")
+   
+try:
+     import requests
+except IOError:
+     os.system("pip2 install requests")
 
-def main():
-    os.system("clear")
-    print(logo)
-    os.system("uname -om")
-    print("")
-    print("Welcome to abm-tool platform ").center(50)
-    knock = raw_input("\tInstall abm_tool with platform (32bit/64bit) ")
-    if knock =="32bit":
-        os.system("cd cool && python2 cool")
-    elif knock =="64bit":
-        os.system("cd tool && python2 tool")
-    else:
-        print("")
-        print("unknow aarch device or invalid system").center(50)
-        print("")
-        time.sleep(1)
-        main()
-        
-if __name__ == '__main__':
-    main()
+import os
+import platform
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from 64bit import main_abm
+    main_abm()
+elif bit == '32bit':
+    from 32bit import main_abm
+    main_abm()     
